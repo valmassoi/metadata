55578b8c7b06d344f059c5468c7f7ccf9f0f4ffe
valmassoi <rvalmassoi@gmail.com>
2016-07-01 15:42:40 -0700

    eslint, code clean up

--- server.js
+++ server.js
@@ -1,50 +1,44 @@
 'use strict'
-const path = require('path')
+
 const express = require('express')
 const http = require('http')
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const fs = require('fs')
 
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${__dirname}/public`))
+
+function clearUploads() {
+  const path = `${__dirname}/uploads/`
+  if (fs.existsSync(path))
+    fs.readdirSync(path).forEach(file => {
+      const curPath = `/${path}/${file}`
+      fs.unlinkSync(curPath)
+    })
+}
 
 app.get('/', (req, res) => {
   res.sendFile('/public/index.html')
 })
 app.post('/uploads', upload.single('file'), (req, res) => {
-  if (req.file){
+  if (req.file) {
     res.json({
       name: req.file.originalname,
-      size: req.file.size
+      size: req.file.size,
     })
     clearUploads()
+  } else {
+    res.json({ error: 'No file to upload, please browse for a file first.' })
   }
-  else
-    res.json({ error: "No file to upload, please browse for a file first." })
 })
 
-app.get("*", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("404!")
+app.get('*', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('404!')
 })
-
-function clearUploads(){
-  var path = __dirname + '/uploads/'
-  if( fs.existsSync(path) ) {
-   fs.readdirSync(path).forEach((file,index) => {
-     let curPath = path + "/" + file
-     if(fs.lstatSync(curPath).isDirectory()) { // recurse
-       deleteFolderRecursive(curPath)
-     } else { // delete file
-       fs.unlinkSync(curPath)
-     }
-   })
-   fs.rmdirSync(path)
-  }
-}
 
 const port = process.env.PORT || 8080
 http.createServer(app).listen(port)
-console.log("Server Running on port: " + port)
+console.log('Server Running on port: ' + port)
